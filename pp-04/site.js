6d8f8c8a3e06f750000d01521d19b3d5757948fa
Stive Tenorio <stivetenorio3520@yahoo.com>
2018-11-12 23:11:55 -0600

    Bonus task two

--- pp-04/site.js
+++ pp-04/site.js
@@ -2,3 +2,9 @@ function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
+
+document.addEventListener('keydown', function(e) {
+  if (e.keyCode == 68) {
+    document.getElementById('controls').play();
+  }
+});
