4d8afc506e50fa4d5822be389f95eb992517730e
Stive Tenorio <stivetenorio3520@yahoo.com>
2018-11-13 00:34:05 -0600

    Finalize js

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,12 +1,26 @@
+"use strict";
+(function(){
+  if (typeof(document.querySelector)==="undefined") {
+    return;
+  }
+  document.addEventListener('DOMContentLoaded', function() {
+
+  document.querySelector('html').className = 'js';
+
 function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
 
-document.addEventListener('keydown', function(e) {
-  if (e.keyCode == 68) {
+document.addEventListener('click', function() {
+     doorbell.play();
+   });
+
+document.addEventListener('keydown', function(d) {
+  if (d.keyCode == 68) {
     document.getElementById('controls').play();
   }
-}
 
-});
+    });
+  });
+})();
