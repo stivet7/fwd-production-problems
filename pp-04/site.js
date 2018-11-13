78ad5937a032e8f3a3eb93764174adb643330f6b
Stive Tenorio <stivetenorio3520@yahoo.com>
2018-11-13 00:45:22 -0600

    eslint fixes

--- pp-04/site.js
+++ pp-04/site.js
@@ -5,21 +5,21 @@
   }
   document.addEventListener('DOMContentLoaded', function() {
 
-  document.querySelector('html').className = 'js';
+    document.querySelector('html').className = 'js';
 
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+    function ringDoorbell() {
+      var doorbell = new Audio('media/doorbell.mp3');
+      doorbell.play();
+    }
 
-document.addEventListener('click', function() {
-     doorbell.play();
-   });
+    document.addEventListener('click', function() {
+      document.getElementById('controls').play();
+    });
 
-document.addEventListener('keydown', function(d) {
-  if (d.keyCode == 68) {
-    document.getElementById('controls').play();
-  }
+    document.addEventListener('keydown', function(d) {
+      if (d.keyCode === 68) {
+        document.getElementById('controls').play();
+      }
 
     });
   });
